44a5820884e6f6412bf2c051d6dd6655af6c50fe
SuperDooJMan <sashasolovev850@gmail.com>
2022-07-07 05:37:44 +0300

    add: 3D test

--- scripts/interactivebody.gd
+++ scripts/interactivebody.gd
@@ -8,16 +8,16 @@ var velocity: Vector2
 var loaded_modules: Array
 
 func _ready() -> void:
-	for module in modules:
-		var loaded_module = module.new()
-		if loaded_module.has_method('setup'):
-			loaded_module.setup(self)
-		loaded_modules.append(loaded_module)
+  for module in modules:
+    var loaded_module = module.new()
+    if loaded_module.has_method('setup'):
+      loaded_module.setup(self)
+    loaded_modules.append(loaded_module)
 
 func _process(delta) -> void:
-	for module in loaded_modules:
-		if module.has_method('exec'):
-			module.exec(delta, self)
+  for module in loaded_modules:
+    if module.has_method('exec'):
+      module.exec(delta, self)
 
-	velocity += Vector2(0, gravity) * Global.get_vector_delta(delta)
-	velocity = move_and_slide(velocity, Vector2.UP)
+  velocity += Vector2(0, gravity) * Global.get_vector_delta(delta)
+  velocity = move_and_slide(velocity, Vector2.UP)
